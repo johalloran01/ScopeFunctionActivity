1d9ee646c176cbcc8e2028c29365e939a016fc25
johalloran01 <joshuamohalloran@gmail.com>
2023-10-11 16:12:42 -0400

    Work on the third task

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -43,24 +43,33 @@ class MainActivity : AppCompatActivity() {
             (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
         else
             sortedList[sortedList.size / 2]
-
         return avg < median
     }
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
+
     private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
-
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
+        return if (recycledView != null) recycledView as TextView else TextView(context).apply {
+            setPadding(5, 10, 10, 0)
+            textSize = 22f
+        } .apply {
+            text.collection[position].toString()
         }
-
-        textView.text = collection[position].toString()
-
-        return textView
     }
+
+
+    //private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
+        //val textView: TextView
+        //if (recycledView != null) {
+          //  textView = recycledView as TextView
+        //} else {
+         //   textView = TextView(context)
+         //   textView.setPadding(5, 10, 10, 0)
+         //   textView.textSize = 22f
+        //}
+
+        //textView.text = collection[position].toString()
+
+        //return textView
+    //}
 
